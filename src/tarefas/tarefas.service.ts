b4244d0589fc415d0c952500766008e228dab761
Gabriel <gabrielbzandrade@gmail.com>
2026-08-07 17:45:04 -0300

    fix: muda o contexto de tarefas de estudo para tarefas de empresa

--- src/tarefas/tarefas.service.ts
+++ src/tarefas/tarefas.service.ts
@@ -1,82 +1,85 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Tarefa, StatusTarefa } from './interfaces/tarefa.interface';
 import { CriarTarefaDto } from './dtos/criar-tarefa-dto';
-import type { AtualizarTarefaDto } from './dtos/atualizar-tarefa-dto';
+import { AtualizarTarefaDto } from './dtos/atualizar-tarefa-dto';
 
 @Injectable()
 export class TarefasService {
-    private tarefas: Tarefa[] = [
-        {
-            id: 1,
-            titulo: 'Estudar NestJS',
-            descricao: 'Desenvolver uma pequena e simples aplicação em NestJS para entender a arquitetura e os conceitos do framework.',
-            status: StatusTarefa.EM_ANDAMENTO
-        },
-        {
-            id: 2,
-            titulo: 'Adicionar Swagger a API',
-            descricao: 'Instalar, configurar e rodar o Swagger na API.',
-            status: StatusTarefa.CONCLUIDA
-        },
-        {
-            id: 3,
-            titulo: 'Estudar Angular',
-            descricao: 'Desenvolver um pequeno e simples projeto frontend em Angular para entender a arquitetura e os conceitos do framework.',
-            status: StatusTarefa.PENDENTE
-        }
-    ]
+  private tarefas: Tarefa[] = [
+    {
+      id: 1,
+      titulo: 'Reabastecer a vitrine',
+      descricao:
+        'Algum funcionário responsável pelo estoque deve realizar uma verificação e reabastecer os produtos que estiverem com poucas unidades na vitrine',
+      status: StatusTarefa.CONCLUIDA,
+    },
+    {
+      id: 2,
+      titulo: 'Empacotar os pedidos para a entrega',
+      descricao:
+        'Algum funcionário do setor de embalagens deve empacotar os pedidos com entrega datada para 11/08/2026',
+      status: StatusTarefa.PENDENTE,
+    },
+    {
+      id: 3,
+      titulo: 'Realizar Balanço',
+      descricao:
+        'O time de finanças deve realizar o balanço geral do mês e trazer os resultados até o dia 08/08/2026',
+      status: StatusTarefa.EM_ANDAMENTO,
+    },
+  ];
 
-    listarTodas(): Tarefa[]{
-        return this.tarefas;
+  listarTodas(): Tarefa[] {
+    return this.tarefas;
+  }
+
+  buscarPorId(id: number): Tarefa {
+    const tarefa = this.tarefas.find((tarefa) => tarefa.id === id);
+
+    if (!tarefa) {
+      throw new NotFoundException('Tarefa não encontrada');
     }
 
-    buscarPorId(id: number): Tarefa{
-        const tarefa = this.tarefas.find((tarefa) => tarefa.id === id);
+    return tarefa;
+  }
 
-        if (!tarefa){
-            throw new NotFoundException("Tarefa não encontrada");
-        }
-        
-        return tarefa;
+  criar(dados: CriarTarefaDto): Tarefa {
+    const novaTarefa: Tarefa = {
+      id: this.tarefas.length + 1,
+      titulo: dados.titulo,
+      descricao: dados.descricao,
+      status: dados.status,
+    };
+
+    this.tarefas.push(novaTarefa);
+
+    return novaTarefa;
+  }
+
+  atualizar(id: number, dados: AtualizarTarefaDto): Tarefa {
+    const tarefa = this.buscarPorId(id);
+
+    if (dados.titulo !== undefined) {
+      tarefa.titulo = dados.titulo;
     }
 
-    criar(dados:CriarTarefaDto): Tarefa{
-        const novaTarefa: Tarefa = {
-            id: this.tarefas.length + 1,
-            titulo: dados.titulo,
-            descricao: dados.descricao,
-            status: dados.status
-        };
-
-        this.tarefas.push(novaTarefa);
-
-        return novaTarefa;
+    if (dados.descricao !== undefined) {
+      tarefa.descricao = dados.descricao;
     }
 
-    atualizar(id:number, dados:AtualizarTarefaDto): Tarefa{
-        const tarefa = this.buscarPorId(id);
-
-        if(dados.titulo !== undefined){
-            tarefa.titulo = dados.titulo;
-        }
-
-        if(dados.descricao !== undefined){
-            tarefa.descricao = dados.descricao;
-        }
-
-        if(dados.status !== undefined){
-            tarefa.status = dados.status;
-        }
-
-        return tarefa;
+    if (dados.status !== undefined) {
+      tarefa.status = dados.status;
     }
 
-    deletar(id:number): {mensagem:string, tarefa:Tarefa}{
-        const tarefa = this.buscarPorId(id);
-        const indiceTarefa = this.tarefas.indexOf(tarefa);
+    return tarefa;
+  }
 
-        this.tarefas.splice(indiceTarefa, 1);
+  deletar(id: number): { mensagem: string; tarefa: Tarefa } {
+    const tarefa = this.buscarPorId(id);
+    const indiceTarefa = this.tarefas.indexOf(tarefa);
 
-        return {mensagem:"Tarefa deletada com sucesso", tarefa:tarefa,};
-    }
+    this.tarefas.splice(indiceTarefa, 1);
+
+    return { mensagem: 'Tarefa deletada com sucesso', tarefa: tarefa };
+  }
 }
