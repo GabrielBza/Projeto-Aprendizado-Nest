37308e9dda5adac10b84018ac74d77ac4221fa68
Gabriel <gabrielbzandrade@gmail.com>
2026-08-07 14:56:51 -0300

    chore: adiciona a estrutura inicial do projeto

---
+++ src/tarefas/interfaces/tarefa.interface.ts
@@ -0,0 +1,12 @@
+export interface Tarefa {
+    id: number;
+    titulo: string;
+    descricao: string;
+    status: StatusTarefa;
+}
+
+export enum StatusTarefa{
+    CONCLUIDA = "CONCLUIDA",
+    EM_ANDAMENTO = "EM_ANDAMENTO",
+    NAO_ATRIBUIDA = "NAO_ATRIBUIDA"
+}
